653eabfd4f8db6453880e095b25bbcd9b0e7e69d
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-06-25 11:32:40 +0200

    create tabls in analysis

---
+++ lib/presentation/controllers/analysis_controller.dart
@@ -0,0 +1,5 @@
+import 'package:budget_app/presentation/controllers/controller_base.dart';
+
+class AnalysisController extends ControllerBase{
+  
+}
--- lib/presentation/pages/analysis_page.dart
+++ lib/presentation/pages/analysis_page.dart
@@ -1,17 +1,29 @@
 import 'package:budget_app/presentation/views/nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AnalysisPage extends StatelessWidget{
   const AnalysisPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: NavBar(),
-      appBar: AppBar(
-        title: const Text('Analysis'),
-      ),
-      body: const Text('Analysis'),
-    );
+    return DefaultTabController(
+      length: 3, 
+      child: Scaffold(
+        drawer: NavBar(),
+        appBar: AppBar(
+          title: const Text('Analysis'),
+          bottom: const TabBar(tabs: [
+            Tab(child: Icon(FontAwesomeIcons.shoppingCart),),
+            Tab(child: Icon(FontAwesomeIcons.handHoldingUsd),),
+            Tab(child: Icon(FontAwesomeIcons.balanceScale,))
+          ]),
+        ),
+        body: TabBarView(children: [
+          Text('expesnses'),
+          Text('Incoms'),
+          Text('balance')
+        ]),)
+      );
   }
 }
